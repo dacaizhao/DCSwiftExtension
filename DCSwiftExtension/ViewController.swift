9ba1d54110da06a465f4ad1beff0e0f6d4233418
app@yangland.com <327532817@qq.com>
2017-01-08 21:18:32 +0800

    first commit

--- DCSwiftExtension/ViewController.swift
+++ DCSwiftExtension/ViewController.swift
@@ -49,38 +49,26 @@ class ViewController: UIViewController {
         
         
         
-        //模型中有模型 数组
-        let dict5 = ["stuNo":101
-            ,"stuClassName":"一年级3班",
-             
-             
-             "classModel":["classID":10,"className":"一年级5班",
-                           "personModel":[
-                            ["age":11,"height":174,"name":"zhaodacai1"],
-                            ["age":12,"height":174,"name":"zhaodacai2"],
-                            ["age":13,"height":174,"name":"zhaodacai3"],
-                            ["age":13,"height":174,"name":"zhaodacai4"],
+        //1:属性有模型 2:模型里有数组 3:StudentModel: PersonModel 可拿父属性
+        let dict5 = ["stuNo":101,"stuClassName":"一年级3班",
+                     "classModel":["classID":10,"className":"一年级5班",
+                                   "personModel":[
+                                                 ["age":11,"height":174,"name":"zhaodacai1"],
+                                                 ["age":12,"height":174,"name":"zhaodacai2"],
+                                                 ["age":13,"height":174,"name":"zhaodacai3"],
+                                                 ["age":13,"height":174,"name":"zhaodacai4"],
                 ]
             ]
             ] as [String : Any]
         
         let studentModel5:StudentModel = StudentModel.dcObjectWithKeyValues(dict5 as NSDictionary)  as! StudentModel
-        
         print("String姓名:\(studentModel5.name)","Int年龄:\(studentModel5.age)","NsNumber身高:\(studentModel5.height)"
             ,"学号:\(studentModel5.stuNo)","班级:\(studentModel5.stuClassName)","模型班级:\(studentModel5.classModel.className)","模型班级Id:\(studentModel5.classModel.classID)")
         print(studentModel5.classModel.personModel.count)
-        
         for model in studentModel5.classModel.personModel as Array {
             print("String姓名:\(model.name)","Int年龄:\(model.age)","NsNumber身高:\(model.height)")
         }
         
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
 }
 
